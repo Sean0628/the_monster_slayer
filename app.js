36455bbb6838b02c21d9ec1a30cb65fd649d60d0
Sho Ito <i.sho.0628@gmail.com>
2019-01-27 18:52:18 +0900

    add heal method

--- app.js
+++ app.js
@@ -30,7 +30,13 @@ new Vue({
 
     },
     heal: function () {
-
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.playerHealth += 10;
+      this.monsterAttacks();
     },
     giveUp: function () {
 
